c59f3bd0ac45d7de8debc46271b5513e384c1565
fjg1989 <2558342483@qq.com>
2017-04-06 23:00:04 +0800

    添加Rxbus

---
+++ app/src/main/java/com/iwangcn/qingkong/rxbus/ThreadMode.java
@@ -0,0 +1,22 @@
+package com.iwangcn.qingkong.rxbus;
+
+
+public enum  ThreadMode {
+
+    /**
+     * current thread
+     */
+    CURRENT_THREAD,
+
+    /**
+     * android main thread
+     */
+    MAIN,
+
+
+    /**
+     * new thread
+     */
+    NEW_THREAD
+
+}
