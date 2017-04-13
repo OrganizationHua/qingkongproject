496b014bda8c2f183dbff6ea76eb400c415c1624
fjg1989 <fengjg@otctop.com>
2017-04-13 18:39:54 +0800

    标签

--- app/src/main/java/com/iwangcn/qingkong/ui/activity/ProcessedActivity.java
+++ app/src/main/java/com/iwangcn/qingkong/ui/activity/ProcessedActivity.java
@@ -1,12 +1,12 @@
 package com.iwangcn.qingkong.ui.activity;
 
+import android.content.Intent;
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
 
 import com.iwangcn.qingkong.R;
 import com.iwangcn.qingkong.ui.adapter.FollowTabAdapter;
 import com.iwangcn.qingkong.ui.base.QkBaseActivity;
-import com.iwangcn.qingkong.utils.ToastUtil;
 
 import butterknife.BindView;
 import butterknife.OnClick;
@@ -44,6 +44,7 @@ public class ProcessedActivity extends QkBaseActivity {
 
     @OnClick(R.id.base_act_right_lin)//APP信息
     public void onSift() {
-        ToastUtil.showToast(this, "查看原文");
+        Intent intent = new Intent(this, TagEditActivity.class);
+        startActivity(intent);
     }
 }
