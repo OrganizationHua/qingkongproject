3ae86722e9de2355734a5063dafaca5e3d075b47
czh <mav2015guo@gmail.com>
2017-04-16 15:32:14 +0800

    登录逻辑增加

--- app/src/main/java/com/iwangcn/qingkong/ui/activity/LoginActivity.java
+++ app/src/main/java/com/iwangcn/qingkong/ui/activity/LoginActivity.java
@@ -1,18 +1,46 @@
 package com.iwangcn.qingkong.ui.activity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.widget.EditText;
 
 import com.iwangcn.qingkong.R;
 import com.iwangcn.qingkong.ui.base.BaseActivity;
+import com.iwangcn.qingkong.utils.ToastUtil;
 
+import butterknife.BindView;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class LoginActivity extends BaseActivity {
+    @BindView(R.id.login_ed_userName)
+    EditText mEdUserName;
+    @BindView(R.id.login_ed_pw)
+    EditText mEdPw;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         ButterKnife.bind(this);
+    }
+
+    @OnClick(R.id.login_btn)//APP信息
+    public void onBtLogin()
+    {
+        String strUserName=mEdUserName.getText().toString().trim();
+        String strPw=mEdPw.getText().toString().trim();
+        if(TextUtils.isEmpty(strUserName)||TextUtils.isEmpty(strPw)){
+            ToastUtil.showToast(this, "用户名和密码不能为空");
+            return;
+        }
+        if(strPw.length()<6){
+            ToastUtil.showToast(this, "密码不能少于6位");
+            return ;
+        }
+        Intent intent=new Intent(this,HomeActivity.class);
+        startActivity(intent);
+        this.finish();
     }
 }
