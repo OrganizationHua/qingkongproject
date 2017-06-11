2a9fb2b8b426d9359c36f50f1f5c79603fa677c5
czh <mav2015guo@gmail.com>
2017-06-11 21:22:20 +0800

    代码优化打包

--- app/src/main/java/com/iwangcn/qingkong/ui/activity/PicActivity.java
+++ app/src/main/java/com/iwangcn/qingkong/ui/activity/PicActivity.java
@@ -1,42 +1,31 @@
 package com.iwangcn.qingkong.ui.activity;
 
-import android.os.Bundle;
-import android.os.PersistableBundle;
 import android.text.TextUtils;
-import android.view.Window;
-import android.view.WindowManager;
 import android.widget.ImageView;
 
 import com.iwangcn.qingkong.R;
-import com.iwangcn.qingkong.ui.base.BaseActivity;
+import com.iwangcn.qingkong.ui.base.QkBaseActivity;
 import com.iwangcn.qingkong.utils.GlideUtils;
 
 import java.util.Arrays;
 import java.util.List;
 
 import butterknife.BindView;
-import butterknife.ButterKnife;
 
-public class PicActivity extends BaseActivity {
+public class PicActivity extends QkBaseActivity {
     @BindView(R.id.img)
     ImageView img;
     String url;
     private List<String> listPic;
 
-
     @Override
-    public void onCreate(Bundle savedInstanceState, PersistableBundle persistentState) {
-        super.onCreate(savedInstanceState, persistentState);
-        requestWindowFeature(Window.FEATURE_NO_TITLE);
-        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
-                WindowManager.LayoutParams.FLAG_FULLSCREEN);
-        setContentView(R.layout.activity_pic);
-        ButterKnife.bind(this);
-        initView();
+    public int layoutChildResID() {
+        return R.layout.activity_pic;
     }
 
-
+    @Override
     public void initView() {
+        setTitle("跟进");
         url = getIntent().getStringExtra("imgurl");
         if (!TextUtils.isEmpty(url)) {
             listPic = Arrays.asList(url.split(","));
